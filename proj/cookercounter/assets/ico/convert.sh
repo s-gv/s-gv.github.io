5d6c22b8d04e701cab834a2ea64dbace8fefd6de
Sagar G V <sagar.writeme@gmail.com>
2015-03-20 16:54:34 +0530

    Teris template

---
+++ proj/cookercounter/assets/ico/convert.sh
@@ -0,0 +1,5 @@
+convert -resize 16x16 ic_launcher-web.png favicon.png
+convert -resize 57x57 ic_launcher-web.png apple-touch-icon-57-precomposed.png
+convert -resize 72x72 ic_launcher-web.png apple-touch-icon-72-precomposed.png
+convert -resize 114x114 ic_launcher-web.png apple-touch-icon-114-precomposed.png
+convert -resize 144x144 ic_launcher-web.png apple-touch-icon-144-precomposed.png
